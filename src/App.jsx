48e10dcae3c7dada6d77457c7f311eddd58d0783
Franz090 <francisoblepias7@gmail.com>
2025-01-27 18:58:49 +0800

    router done setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import ErrorBoundary from './pages/ErrorBoundary';
+import { BrowserRouter as Router } from "react-router-dom";
+import './App.css';
+import Routers from './routes/Routers';
+
 function App() {
- 
   return (
     <Router>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      {/* This will match any route that is not defined */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>
-  )
+      <Routers />
+    </Router>
+  );
 }
 
-export default App
+export default App;
